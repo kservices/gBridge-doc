c132bf8fdf694fcfe1e036720d34c7e852b8c834
Peter Kappelt <kappelt.peter@gmail.com>
2018-12-28 12:34:22 +0100

    Configured pages for Travis builds

---
+++ ghpages-push.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+ORG=kservices
+REPO=gBridge-doc
+EMAIL=kappelt.peter@gmail.com
+
+set -e
+
+cd ..
+git clone -b gh-pages "https://$GH_TOKEN@github.com/$ORG/$REPO.git" gh-pages
+cd gh-pages
+
+if [ "$1" != "dry" ]; then
+    git config user.name "Documentation Builder (Travis)"
+    git config user.email "$EMAIL"
+fi
+
+cp -R ../$REPO/_build/html/* ./
+
+git add -A .
+git commit -m "[CI] Sphinx commit for $COMMIT."
+if [ "$1" != "dry" ]; then
+    git push -q origin gh-pages
+fi
